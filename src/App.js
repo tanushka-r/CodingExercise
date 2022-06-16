d2f3c61516ff68313f2ab4addc1bac01a00a0d59
Tania Roche <littlestar@TANIAS-MBP.station>
2022-06-16 17:49:06 +0100

    Installing React and adding initial set up, converting css into scss

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+
+const App = () => {
+  return (
+    <Fragment>
+      <div className="exercise-header">
+        <h2>Exercise 1 - Testimonial Block</h2>
+      </div>
+      <div>
+        <div>
+          <div>
+            <p>Gingerbread tart cupcake cake muffin cookie liquorice tiramisu. Toffee cupcake cake cake croissant icing carrot cake cookie. Dessert chocolate bar apple pie sesame snaps liquorice carrot cake cookie danish.</p>
+            <span>Indiana Jones, Archaeologist</span>
+          </div>
+          <a>Tell Me More</a>
+        </div>
+      </div>
+      <div className="exercise-header">
+        <h2>Exercise 2 - Filterable Content</h2>
+      </div>
+      <div>[Exercise 2 Here]</div>
+    </Fragment>
+  );
+};
+
+export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
-import '../styles/index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-console.log('App Ready');
+import App from '../App';
+import '../styles/index.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
